a59c12d698918534895f1d647f5a0928658beea7
MookMao <18896940212@163.com>
2020-10-20 21:36:37 +0800

    第一周：自定义ClassLoader加载Hello.xlass

---
+++ Week_01/src/HelloClassloader.java
@@ -0,0 +1,47 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * 自定义一个 Classloader，加载一个 Hello.xlass 文件，执行 hello 方法，
+ * 此文件内容是一个 Hello.class 文件所有字节（x=255-x）处理后的文件
+ */
+public class HelloClassloader extends ClassLoader {
+    public static void main(String[] args) {
+        try {
+            Class<?> aClass = new HelloClassloader().findClass("Hello");
+            Object obj = aClass.newInstance();
+            Method method = aClass.getMethod("hello");
+            method.invoke(obj);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        File f = new File("Hello.xlass");
+        int length = (int)f.length();
+        byte[] bytes = new byte[length];
+        try {
+            new FileInputStream(f).read(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return super.findClass(name);
+        }
+        for (int i = 0; i < bytes.length; ++i) {
+            bytes[i] = (byte)(255 - bytes[i]);
+        }
+        return defineClass(name, bytes, 0, bytes.length);
+    }
+}
